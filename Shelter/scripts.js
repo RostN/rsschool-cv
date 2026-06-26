85ff3f7b95f7333795b9e5254686b8f9215a633d
Rostislav <133744981+RostN@users.noreply.github.com>
2026-06-26 16:59:27 +0700

    fix: script

--- Shelter/scripts.js
+++ Shelter/scripts.js
@@ -1,36 +1,57 @@
-let grade = 
-"Main page\n"+
-"1. Markup validation - 10 / 10 \n" +
-" - Markup is valid 5 / 5 \n"+
-" - The header logo is built from text elements, the page contains exactly one <h1>, and a favicon is added 5/5\n"+
-"2. Layout matches the design - 35 / 35\n"+
-" - <header> block 5/5\n"+
-" - Not only block 5/5\n"+
-" - About block 5/5\n"+
-" - Our Friends block 5/5\n"+
-" - Help block 5/5\n"+
-" - In addition block 5/5\n"+
-" - <footer> block 5/5\n"+
-"3. CSS requirements - 15 / 15\n"+
-" - The Help block is positioned using a grid layout (flexbox or grid) 5/5\n"+
-" - Centering the page when scaling 5/5\n"+
-" - The background color stretches across the full page width 5/5\n"+
-"4. Interactivity - 10 /10\n"+
-" - The About the Shelter navigation item is highlighted and non-interactive; the other navigation items are interactive; smooth anchor scrolling works; and all page links behave per the Page links and navigation section 5/5\n"+
-" - Each pet card in Our Friends is interactive when hovering over any area of the card; links and buttons have hover/active styling beyond cursor: pointer (color/background change); visual changes are smooth and do not affect neighboring elements 5/5\n"+
-"Pets page\n"+
-"1. Markup validation - 10 / 10 \n" +
-" - Markup is valid 5 / 5 \n"+
-" - The header logo is built from text elements, the page contains exactly one <h1>, and a favicon is added 5/5\n"+
-"2. Layout matches the design - 15 / 15\n"+
-" - <header> block 5/5\n"+
-" - Our friends block 5/5\n"+
-" - <footer> block 5/5\n"+
-"3. CSS requirements - 5 / 5\n"+
-" - Centering the page when scaling 5/5\n"+
-"4. Interactivity - 10 /10\n"+
-" - The Our pets navigation item is highlighted and non-interactive; the other navigation items are interactive; smooth anchor scrolling works; and all page links behave per the Page links and navigation section 5/5\n"+
-" - Each pet card in Our Friends is interactive when hovering over any area of the card; links and buttons have hover/active styling beyond cursor: pointer (color/background change); visual changes are smooth and do not affect neighboring elements 5/5\n"+
-"\n Total score: 110 / 110";
+/*  */
+/*  */
 
-console.log(grade);
+
+/* Получение данных из JSON файла */
+let sliderCart = document.querySelector('.pets-content-slider-data');
+let petsCartP1 = '<div class="pets-content-slider-data-cart"><img src="';
+// + картинка питомца
+let petsCartP2 = '" alt=""><div class="pets-cart-text">';
+// + Имя петомца
+let petsCartP3 = '</div><button class="pets-cart-button">Learn more</button></div>';
+
+    fetch('pets.json')
+      .then(response => {
+        if (!response.ok) {
+            throw new Error('Ой, ошибка в fetch: ' + response.statusText);
+        } return response.json();})
+      .then(jsonData => {
+        datajson = jsonData;
+        console.log(datajson[0]);
+        createPetsCart()
+        return jsonData;
+        })
+        .catch(error => console.error('Ошибка при исполнении запроса: ', error));
+
+    function createPetsCart(){
+        testcart = petsCartP1+datajson[0].img + petsCartP2 + datajson[0].name + petsCartP3;
+        sliderCart.insertAdjacentHTML('beforeend', testcart);
+    }
+    
+/* Контроль изменения размера окна */
+    window.addEventListener('resize', () => {
+        console.log('Изменение экрана');
+        hideNavMenu(); //Проверка размера экрана
+    });
+
+/* Бургер */
+let burger = document.querySelector ('.burger');
+let navMenu = document.querySelector('.header-nav');
+
+    // Функция смены рисунка бургера (cлушатель) и отображение боковой панели
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('change'); 
+        navMenu.classList.toggle('navOn');
+        // console.log(datajson[0].img);
+    });
+
+    // Функция сокрытия панели меню при малом размере экрана
+    function hideNavMenu(){
+        console.log('Расширение экрана: ', window.innerWidth)
+        if (window.innerWidth < 768){
+            navMenu.classList.toggle('.hide');
+        }
+    }
+
+    // Сокрытие панели меню при малом размере экрана
+    hideNavMenu();
