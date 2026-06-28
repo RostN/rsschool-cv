d296c84fa8b52172d1b1dc4cff3908d0c6b6f368
Rostislav <133744981+RostN@users.noreply.github.com>
2026-06-29 02:45:16 +0300

    fix: all files (Mon, June 29, 2026 06:45 AM)

--- Shelter/scripts.js
+++ Shelter/scripts.js
@@ -1,14 +1,36 @@
 /*  */
-/*  */
 
+/* Пагинатор */
+let indexSliderPrev = document.getElementById('indexSliderPrev');
+let indexSliderNext = document.getElementById('indexSliderNext');
+let indexSliderPosition = 0;
+
+    // Слушатель кнопки назад
+    indexSliderPrev.addEventListener('click', function(){
+        updateCarts(100);
+    })
+
+    // Слушатель кнопки вперёд
+    indexSliderNext.addEventListener('click', function(){
+        updateCarts(-100);
+    })
+
+    // Обновление слайдера
+    function updateCarts(a){
+        indexSliderPosition += a;
+        sliderCartInd.style.transform =
+        `translate(${indexSliderPosition}px, 0px)`;
+    }
 
 /* Получение данных из JSON файла */
-let sliderCart = document.querySelector('.pets-content-slider-data');
-let petsCartP1 = '<div class="pets-content-slider-data-cart"><img src="';
+let sliderCartInd = document.querySelector('.slider-container');
+let petsCartP1 = '<div class="pets-content-slider-data-cart index-page"><img src="';
 // + картинка питомца
 let petsCartP2 = '" alt=""><div class="pets-cart-text">';
 // + Имя петомца
 let petsCartP3 = '</div><button class="pets-cart-button">Learn more</button></div>';
+let slidesInd = Array.from(sliderCartInd.children);
+let slidesGap = 0;
 
     fetch('pets.json')
       .then(response => {
@@ -17,15 +39,32 @@ let petsCartP3 = '</div><button class="pets-cart-button">Learn more</button></di
         } return response.json();})
       .then(jsonData => {
         datajson = jsonData;
-        console.log(datajson[0]);
+        // console.log(datajson[0]);
         createPetsCart()
         return jsonData;
         })
         .catch(error => console.error('Ошибка при исполнении запроса: ', error));
 
+    // Функция создания карточекы
+    let testcart = '';
+
+    
     function createPetsCart(){
-        testcart = petsCartP1+datajson[0].img + petsCartP2 + datajson[0].name + petsCartP3;
-        sliderCart.insertAdjacentHTML('beforeend', testcart);
+        // testcart = petsCartP1+datajson[0].img + petsCartP2 + datajson[0].name + petsCartP3;
+        for ( i = 0; i < 8; i++){
+            testcart = testcart + 
+            petsCartP1+
+            datajson[i].img +
+            petsCartP2 +
+            datajson[i].name +
+            petsCartP3;
+            // console.log(i);
+        }
+        sliderCartInd.insertAdjacentHTML('beforeend', testcart);
+        slidesInd = Array.from(sliderCartInd.children); //Массив созданных элементов
+        // slidesGap = slidesInd[0].clientWidth + 22.5;
+        sliderCartInd.style.gap = 40;
+        console.log(slidesInd[0].clientWidth);
     }
     
 /* Контроль изменения размера окна */
@@ -42,7 +81,6 @@ let navMenu = document.querySelector('.header-nav');
     burger.addEventListener('click', function() {
         burger.classList.toggle('change'); 
         navMenu.classList.toggle('navOn');
-        // console.log(datajson[0].img);
     });
 
     // Функция сокрытия панели меню при малом размере экрана
