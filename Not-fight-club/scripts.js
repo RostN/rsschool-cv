1f4006f5019f050cc44fb289bfd57ce1d238a4b4
Rostislav <133744981+RostN@users.noreply.github.com>
2026-07-17 20:50:44 +0700

    fix: update
    add modal
    add color of choose img

--- Not-fight-club/scripts.js
+++ Not-fight-club/scripts.js
@@ -16,12 +16,38 @@ let mainPageBtnFight = document.querySelector('.main-page-btnFight');
 // Страница аккаунта
 let accountPage = document.querySelector('.account-page');
 let accountInfo = document.querySelector('.account-page-info');
+let accountAva = document.querySelector('.account-page-ava');
+let accountCountWin = document.querySelector('.account-page-info-wins-count');
+let accountCountLose = document.querySelector('.account-page-info-lose-count');
+let dialog = document.getElementById('dialog');
+let exitBtn = document.querySelector('.account-page-galary-btns-exit');
+let acceptBtn = document.querySelector('.account-page-galary-btns-accept');
+let avas = document.querySelectorAll('.account-page-galary-ava');
 
 // Страница настроек
 let settingPage = document.querySelector('.setting-page');
 let playerName = document.querySelector('.setting-page-player-name');
 
-/* Исполняемый код */ 
+
+/* Исполняемый код */
+avas.forEach(e => {
+    e.addEventListener('click', function(){
+        avas.forEach(el => el.classList.remove('account-page-galary-ava-selected'))
+        e.classList.add('account-page-galary-ava-selected');
+        console.log(e.classList);
+    })
+})
+
+// Кнопка закрытия в модалке
+exitBtn.addEventListener('click', function(){
+    dialog.close();
+})
+
+// Клик на аватарку открытие модалки для изменения аватарки
+accountAva.addEventListener('click', function(){
+    dialog.showModal();
+})
+
 // Кнопка настройки
 btnSetting.addEventListener('click', function(){
     accountPage.style.display = 'none'; // Сокрытие страницы аккаунт
@@ -32,11 +58,17 @@ btnSetting.addEventListener('click', function(){
 
 // Кнопка аккаунт
 btnAccount.addEventListener('click', function(){
+    let img = document.createElement('img'); // Создание элемента с изображением
     mainPage.classList.add('hide'); // Сокрытие основной страницы
     settingPage.classList.add('hide'); // Сокрытие страницы настроек
     pageName.textContent = 'Character'; // Смена названия страницы
     accountPage.style.display = 'flex'; // Отображение страницы аккаунт
-    accountInfo.textContent = 'Statistic'; // Заполнение поле с информацией
+    
+    accountCountLose.append('5');
+    img.src = 'img/p1.png'; // Ссылка на картинку
+    accountAva.appendChild(img); // Вставка изображения в аватарку
+    accountCountWin.textContent = '999';
+    accountCountLose.textContent = '000';
 })
  
 // Кнопка домой
@@ -49,17 +81,15 @@ btnHome.addEventListener('click', function(){
 
 // Кнопка создания персонажа
 startBtnCreate.addEventListener('click', function(){
+    startPage.classList.toggle('hide'); // Отключить стартовую страницу
+    mainPage.classList.remove('hide'); // Отображение стартовой страницы
     head.classList.toggle('hide'); // Отображение заголовка
-    startPage.classList.toggle('hide'); //Отключить стартовую страницу
-    mainPage.classList.remove('hide');
 })
 
-// Изменение названия надписи в залоголовке
-// pageName.textContent = 'Page'
-
-// Сокрытия заголовка на старте
-head.classList.add('hide'); //Сокрытия header
-accountPage.style.display = 'none'; //Сокрытие окна аккаунта.
+// Сокрытия лишнего на старте
+// head.classList.add('hide'); //Сокрытия header
+// accountPage.style.display = 'none'; // Сокрытие окна аккаунта.
 settingPage.classList.add('hide'); // Сокрытие страницы настроек
 mainPage.classList.add('hide'); // Сокрытие основной страницы
-// startPage.classList.add('hide'); // Временно Сокрытие стартовой страницы
+
+startPage.classList.add('hide'); // Временно Сокрытие стартовой страницы
