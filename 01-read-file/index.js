f8e36b146bbfac1b5104cda3bf5db83606cf5c8d
Rostislav <133744981+RostN@users.noreply.github.com>
2026-08-05 21:30:36 +0700

    fix: finish

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
 
+const readStream = fs.createReadStream('01-read-file/text.txt', { encoding: 'utf8' });
+
+readStream.on('data', (chunk) => {
+  // Вывод строки
+  process.stdout.write(chunk);
+  // Отступ в конце
+  console.log('\n'); 
+});
+
+readStream.on('error', (err) => {
+  console.error('Ошибка при чтении файла:', err);
+});
